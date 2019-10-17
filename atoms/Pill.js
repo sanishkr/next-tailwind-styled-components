cc83a6e54caa2436173ca6801d22f50d3bc73643
Sanish <skumar1@tila.com>
2019-10-17 23:33:36 +0530

    :alembic: testing conditional spacing based on breakpoints

--- atoms/Pill.js
+++ atoms/Pill.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import tw from "tailwind.macro";
 
 const StyledPill = styled.span`
-	${tw`sm:mb-2 md:mb-2 inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2`};
+	${tw`mb-2 xl:mb-0 inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2`};
 `;
 
 export default ({ text }) => {
