9678bcd244d9a21e085084ec7dcc4fc555172b0e
Sanish <skumar1@tila.com>
2019-10-17 23:34:37 +0530

    :alembic: trying prop based styled components

--- components/ArticleCard.js
+++ components/ArticleCard.js
@@ -5,14 +5,15 @@ import CardBody from "../molecules/CardBody";
 import Pill from "../atoms/Pill";
 
 const Card = styled.div`
-	${tw`w-full sm:w-full md:w-1/2 lg:w-1/3 xl:w-1/4 max-w-sm`};
+	${tw`mb-4 mx-2 bg-white overflow-hidden shadow-lg`};
+	${props => (props.rounded ? tw`rounded` : ``)};
 `;
 
 export default ({ data }) => {
 	const { id, title, description, imgUrl, imgAlt, tags } = data;
 	return (
-		<Card>
-			<div css={tw`mb-4 mx-2 bg-white rounded overflow-hidden shadow-lg`}>
+		<div css={tw`w-full sm:w-full md:w-1/2 lg:w-1/3 xl:w-1/4 max-w-sm`}>
+			<Card rounded>
 				<img css={tw`w-full`} src={imgUrl} alt={imgAlt} />
 				<CardBody title={title} description={description}></CardBody>
 				<div css={tw`px-6 py-4`}>
@@ -20,7 +21,7 @@ export default ({ data }) => {
 						<Pill key={i} text={`#${tag}`}></Pill>
 					))}
 				</div>
-			</div>
-		</Card>
+			</Card>
+		</div>
 	);
 };
