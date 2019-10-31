d727d914e95219eb0a23cb3fc81bc75e3f7d1a8a
Sanish <skumar1@tila.com>
2019-10-31 16:17:30 +0530

    :alembic: tailwindcss-dir plugin class with tw macro and styled-components

--- pages/page1.js
+++ pages/page1.js
@@ -2,9 +2,17 @@ import styled from "styled-components";
 import tw from "tailwind.macro";
 import "../assets/css/pages/page1.css";
 
-const Container = styled.div`
-	${tw`bg-black min-h-screen flex flex-col items-center justify-center text-xl`};
+const Container = styled.div.attrs({ className: "ltr:mx-4 rtl:mx-8" })`
+	${tw`bg-gray-800 min-h-screen flex flex-col items-center justify-center text-xl`};
 	margin: 10px;
+	[dir=ltr] .ltr\:mx-4 {
+    margin-left: 1rem;
+    margin-right: 1rem;
+	}
+	[dir=rtl] .rtl\:mx-8 {
+    margin-left: 2rem;
+    margin-right: 2rem;
+	}
 `;
 
 export default () => {
