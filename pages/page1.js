a21f09dc8de81bb166e7cd6e8540584ebf049934
sanish <sanishkr@gmail.com>
2019-11-01 02:05:24 +0530

    :alembic: added example to use direction based critical css in styled components

--- pages/page1.js
+++ pages/page1.js
@@ -2,16 +2,16 @@ import styled from "styled-components";
 import tw from "tailwind.macro";
 import "../assets/css/pages/page1.css";
 
-const Container = styled.div.attrs({ className: "ltr:mx-4 rtl:mx-8" })`
+const Container = styled.div.attrs({ dir: "rtl" })`
 	${tw`bg-gray-800 min-h-screen flex flex-col items-center justify-center text-xl`};
 	margin: 10px;
-	[dir=ltr] .ltr\:mx-4 {
-    margin-left: 1rem;
-    margin-right: 1rem;
+	&[dir="ltr"] {
+		margin-left: 1rem;
+		margin-right: 1rem;
 	}
-	[dir=rtl] .rtl\:mx-8 {
-    margin-left: 2rem;
-    margin-right: 2rem;
+	&[dir="rtl"] {
+		margin-left: 2rem;
+		margin-right: 2rem;
 	}
 `;
 
