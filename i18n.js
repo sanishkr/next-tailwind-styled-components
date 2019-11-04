2716b4f76629d41d2acb7683faaa620d84803798
Sanish <skumar1@tila.com>
2019-11-04 21:27:39 +0530

    :heavy_plus_sign: adding next-i18next for internationalisation

---
+++ i18n.js
@@ -0,0 +1,11 @@
+const NextI18Next = require('next-i18next').default;
+
+module.exports = new NextI18Next({
+	defaultLanguage: 'en',
+	otherLanguages: ['ar'],
+	localePath: typeof window === 'undefined' ? 'public/locales' : 'locales',
+	localeSubpaths: {
+		en: 'en',
+		ar: 'ar'
+	}
+});
