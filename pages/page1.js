31c7137b4262c8762ed9f464035737c17543b4b5
Sanish <skumar1@tila.com>
2019-11-04 21:28:43 +0530

    :globe_with_meridians: adding header and nookies to page1

--- pages/page1.js
+++ pages/page1.js
@@ -1,15 +1,21 @@
-import styled from "styled-components";
-import tw from "tailwind.macro";
-import "../assets/css/pages/page1.css";
+import styled from 'styled-components';
+import tw from 'tailwind.macro';
 
-const Container = styled.div.attrs({ dir: "rtl" })`
+import { i18n } from '../i18n';
+import Header from '../components/Header';
+
+import '../assets/css/pages/page1.css';
+
+const dir = i18n.language === 'ar' ? 'rtl' : 'ltr';
+
+const Container = styled.div.attrs({ dir })`
 	${tw`bg-gray-800 min-h-screen flex flex-col items-center justify-center text-xl`};
 	margin: 10px;
-	&[dir="ltr"] {
+	&[dir='ltr'] {
 		margin-left: 1rem;
 		margin-right: 1rem;
 	}
-	&[dir="rtl"] {
+	&[dir='rtl'] {
 		margin-left: 2rem;
 		margin-right: 2rem;
 	}
@@ -17,14 +23,17 @@ const Container = styled.div.attrs({ dir: "rtl" })`
 
 export default () => {
 	return (
-		<div css={tw`text-center`}>
-			<Container>
-				Hello, World!!
-				<p css={tw`text-blue-300`}>
-					I'm using <code>tailwind</code> and <code>styled-components</code>{" "}
-					together.
-				</p>
-			</Container>
-		</div>
+		<>
+			<Header></Header>
+			<div css={tw`text-center`}>
+				<Container>
+					Hello, World!!
+					<p css={tw`text-blue-300`}>
+						I'm using <code>tailwind</code> and <code>styled-components</code>{' '}
+						together.
+					</p>
+				</Container>
+			</div>
+		</>
 	);
 };
