fa34bebcdfd057da331a002bbf433647be9aaeb4
Sanish <skumar1@tila.com>
2020-02-07 23:23:16 +0530

    :globe_with_meridians: update routes without lang

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@ const routes = require('next-routes')();
 routes
   .add({ name: 'page1', pattern: '/page1', page: 'page1' })
   .add({ name: 'page2', pattern: '/page2', page: 'page2' })
-  .add({ name: '404', pattern: '/:language(en|ar)/404', page: '404' })
-  .add({ name: 'default', pattern: '/:language(en|ar)?', page: 'home' });
+  .add({ name: '404', pattern: '/404', page: '404' })
+  .add({ name: 'default', pattern: '/', page: 'home' });
 
 module.exports = routes;
